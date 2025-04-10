0931d904545e17a97738c99e9a69ecb4b84866fe
GavinEscanilla <gavinaltonescanilla@gmail.com>
2025-04-09 23:17:10 -0400

    start

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App bg-red-900 h-screen">
+      Boiler plate
+    </div>
+  )
+}
+
+export default App
